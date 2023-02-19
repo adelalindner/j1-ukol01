255cfcec397f742718f242f94cc71f4a9ce89738
AdelaLindner <a.lindnerova@seznam.cz>
2023-02-19 14:32:52 +0100

    nakresliPrasatko

--- src/main/java/cz/czechitas/java/ukol01/HlavniProgram.java
+++ src/main/java/cz/czechitas/java/ukol01/HlavniProgram.java
@@ -7,9 +7,67 @@ public class HlavniProgram {
     public void start() {
         Turtle zofka = new Turtle();
 
+        /*
+
         nakresliPrasatko(zofka);
 
+        zofka.penUp();
+        zofka.turnLeft(30);
+        zofka.move(200);
+        zofka.turnRight(90);
+        zofka.penDown();
 
+        nakresliKruznici(zofka);
+
+        zofka.penUp();
+        zofka.turnLeft(180);
+        zofka.move(200);
+        zofka.turnLeft(180);
+        zofka.penDown();
+
+
+        nakresliSlunce(zofka);
+
+        zofka.penUp();
+        zofka.turnLeft(180);
+        zofka.move(200);
+        zofka.penDown();
+
+        nakresliOsmiuhelnik(zofka);
+
+        */
+
+
+
+    }
+
+    private static void nakresliOsmiuhelnik(Turtle zofka) {
+        for (int i = 0; i < 8; i++) {
+            zofka.move(50);
+            zofka.turnRight(45);
+        }
+    }
+
+    private static void nakresliSlunce(Turtle zofka) {
+        for (int i = 0; i < 12; i++) {
+
+            for (int j = 0; j < 30; j++) {
+                zofka.move(1);
+                zofka.turnLeft(1);
+            }
+                zofka.turnRight(90);
+                zofka.move(25);
+                zofka.turnRight(180);
+                zofka.move (25);
+                zofka.turnRight(90);
+        }
+    }
+
+    private static void nakresliKruznici(Turtle zofka) {
+        for (int i = 0; i < 360; i++) {
+            zofka.move(1);
+            zofka.turnLeft(1);
+        }
     }
 
     private static void nakresliPrasatko(Turtle zofka) {
